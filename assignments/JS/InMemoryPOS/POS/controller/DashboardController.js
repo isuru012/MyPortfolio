82026d373446d1e60715ac62917bf44c5500de6b
Isuru <isurudulakshana012@gmail.com>
2023-06-18 23:31:09 +0530

    some portfolio changes

---
+++ assignments/JS/InMemoryPOS/POS/controller/DashboardController.js
@@ -0,0 +1,139 @@
+
+function anchorClick(anchorId) {
+
+    if (anchorId === 'dashboard') {
+        document.getElementById('dash').style.setProperty("background-color", "#BFF5DC", "important");
+        document.getElementById('cus').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('itm').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('ordrs').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('newOrdrs').style.setProperty("background-color", "#ffffff", "important");
+
+        document.getElementById('dash').style.setProperty("color", "#03a293");
+        document.getElementById('cus').style.setProperty("color", "#000000");
+        document.getElementById('itm').style.setProperty("color", "#000000");
+        document.getElementById('ordrs').style.setProperty("color", "#000000");
+        document.getElementById('newOrdrs').style.setProperty("color", "#000000");
+
+        const mq = window.matchMedia("(max-width:900px)");
+
+        if (mq.matches) {
+
+            document.getElementById("wrapper").classList.toggle("toggled");
+
+        }
+
+
+    } else if (anchorId === 'customers') {
+        document.getElementById('dash').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('cus').style.setProperty("background-color", "#BFF5DC", "important");
+        document.getElementById('itm').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('ordrs').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('newOrdrs').style.setProperty("background-color", "#ffffff", "important");
+
+        document.getElementById('dash').style.setProperty("color", "#000000");
+        document.getElementById('cus').style.setProperty("color", "#03a293");
+        document.getElementById('itm').style.setProperty("color", "#000000");
+        document.getElementById('ordrs').style.setProperty("color", "#000000");
+        document.getElementById('newOrdrs').style.setProperty("color", "#000000");
+        const mq = window.matchMedia("(max-width:900px)");
+
+        if (mq.matches) {
+            document.getElementById("wrapper").classList.toggle("toggled");
+
+        }
+
+    } else if (anchorId === 'items') {
+        document.getElementById('dash').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('cus').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('itm').style.setProperty("background-color", "#BFF5DC", "important");
+        document.getElementById('ordrs').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('newOrdrs').style.setProperty("background-color", "#ffffff", "important");
+
+        document.getElementById('dash').style.setProperty("color", "#000000");
+        document.getElementById('cus').style.setProperty("color", "#000000");
+        document.getElementById('itm').style.setProperty("color", "#03a293");
+        document.getElementById('ordrs').style.setProperty("color", "#000000");
+        document.getElementById('newOrdrs').style.setProperty("color", "#000000");
+
+        const mq = window.matchMedia("(max-width:900px)");
+
+        if (mq.matches) {
+            document.getElementById("wrapper").classList.toggle("toggled");
+
+        }
+
+    } else if (anchorId === 'orders') {
+        document.getElementById('dash').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('cus').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('itm').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('ordrs').style.setProperty("background-color", "#BFF5DC", "important");
+        document.getElementById('newOrdrs').style.setProperty("background-color", "#ffffff", "important");
+
+        document.getElementById('dash').style.setProperty("color", "#000000");
+        document.getElementById('cus').style.setProperty("color", "#000000");
+        document.getElementById('itm').style.setProperty("color", "#000000");
+        document.getElementById('ordrs').style.setProperty("color", "#03a293");
+        document.getElementById('newOrdrs').style.setProperty("color", "#000000");
+        const mq = window.matchMedia("(max-width:900px)");
+
+        if (mq.matches) {
+            document.getElementById("wrapper").classList.toggle("toggled");
+
+
+        }
+    } else if (anchorId === 'newOrder') {
+        document.getElementById('dash').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('cus').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('itm').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('ordrs').style.setProperty("background-color", "#ffffff", "important");
+        document.getElementById('newOrdrs').style.setProperty("background-color", "#BFF5DC", "important");
+
+        document.getElementById('dash').style.setProperty("color", "#000000");
+        document.getElementById('cus').style.setProperty("color", "#000000");
+        document.getElementById('itm').style.setProperty("color", "#000000");
+        document.getElementById('ordrs').style.setProperty("color", "#000000");
+        document.getElementById('newOrdrs').style.setProperty("color", "#03a293");
+
+        const mq = window.matchMedia("(max-width:900px)");
+
+        if (mq.matches) {
+            document.getElementById("wrapper").classList.toggle("toggled");
+
+        }
+
+    }
+}
+
+
+function loadSection(sectionId) {
+    const sections = ['dashboard', 'customers', 'items', 'orders', 'newOrder'];
+
+    sections.forEach(function (section) {
+        const display = section === sectionId ? 'block' : 'none';
+        document.getElementById(section).style.display = display;
+    });
+
+    const sectionTitles = {
+        dashboard: 'Dashboard',
+        customers: 'Customers',
+        items: 'Items',
+        orders: 'Orders',
+        newOrder: 'NewOrder',
+    };
+
+    document.getElementById('h2Ele').textContent = sectionTitles[sectionId];
+
+    localStorage.setItem('activeSection', sectionId);
+
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const lastActiveSection = localStorage.getItem('activeSection');
+    if (lastActiveSection) {
+        loadSection(lastActiveSection);
+    } else {
+        // Set a default section if no active section is found
+        loadSection('dashboard');
+    }
+});
