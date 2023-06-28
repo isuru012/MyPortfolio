6b7a4419794f319312a0183fdb5fcb222f7b1e93
Isuru Dulakshana <isurudulakshana012@gmail.com>
2023-06-28 22:40:56 +0530

    done some

--- assignments/JS/InMemoryPOS/POS/controller/CustomerController.js
+++ assignments/JS/InMemoryPOS/POS/controller/CustomerController.js
@@ -109,5 +109,140 @@ $(document).ready(function() {
             selectedRow = null;
         }
     });
+
+    const inputName = document.querySelector("#txtName");
+    const inputAddress = document.querySelector("#txtAddress");
+    const inputPhoneNumber = document.querySelector("#txtPhoneNumber");
+
+    inputName.addEventListener("input", function() {
+        const value = inputName.value;
+        const regex = new RegExp("^[a-zA-Z ]+$");
+
+        if (!regex.test(value)) {
+            inputName.style.borderColor = "red";
+        } else {
+            inputName.style.borderColor = "black";
+        }
+    });
+
+    inputAddress.addEventListener("input", function() {
+        const value = inputAddress.value;
+        const regex = new RegExp("^[a-zA-Z0-9 ,.-]+$");
+
+        if (!regex.test(value)) {
+            inputAddress.style.borderColor = "red";
+        } else {
+            inputAddress.style.borderColor = "black";
+        }
+    });
+
+    inputPhoneNumber.addEventListener("input", function() {
+        const value = inputPhoneNumber.value;
+        const regex = new RegExp("^(091|071|077|076|075|070)[0-9]{7}$");
+
+        if (!regex.test(value)) {
+            inputPhoneNumber.style.borderColor = "red";
+        } else {
+            inputPhoneNumber.style.borderColor = "black";
+        }
+    });
+
+    /*function validateName() {
+        var nameInput = $('.txtName');
+        var name = nameInput.val();
+        var namePattern = /^[A-Za-z\s]+$/;
+        var isValidName = namePattern.test(name);
+
+        if (isValidName) {
+            nameInput.removeClass("invalid");
+        } else {
+            nameInput.addClass("invalid");
+        }
+    }
+
+    // Function to validate the address input
+    function validateAddress() {
+        var addressInput = $('.txtAddress');
+        var address = addressInput.val();
+        var addressPattern = /^[A-Za-z0-9\s]+$/;
+        var isValidAddress = addressPattern.test(address);
+
+        if (isValidAddress) {
+            addressInput.removeClass("invalid");
+        } else {
+            addressInput.addClass("invalid");
+        }
+    }
+
+    // Function to validate the phone number input
+    function validatePhoneNumber() {
+        var phoneNumberInput = $('.txtPhoneNumber');
+        var phoneNumber = phoneNumberInput.val();
+        var phoneNumberPattern = /^(091|071|077|076|075|070)\d{7}$/;
+        var isValidPhoneNumber = phoneNumberPattern.test(phoneNumber);
+
+        if (isValidPhoneNumber) {
+            phoneNumberInput.removeClass("invalid");
+        } else {
+            phoneNumberInput.addClass("invalid");
+        }
+    }
+
+    // Add event listeners for real-time validation
+    $('.txtName').on('input', validateName);
+    $('.txtAddress').on('input', validateAddress);
+    $('.txtPhoneNumber').on('input', validatePhoneNumber);*/
+
+    // Add click event listener for the "Add" button
+    $('#add1').click(function () {
+        var nameInput = $('.txtName');
+        var addressInput = $('.txtAddress');
+        var phoneNumberInput = $('.txtPhoneNumber');
+
+        var name = nameInput.val();
+        var address = addressInput.val();
+        var phoneNumber = phoneNumberInput.val();
+
+        // Regular expressions for validation
+        var namePattern = /^[A-Za-z\s]+$/;
+        var addressPattern = /^[A-Za-z0-9\s]+$/;
+        var phoneNumberPattern = /^(091|071|077|076|075|070)\d{7}$/;
+
+        // Validate name, address, and phone number
+        var isValidName = namePattern.test(name);
+        var isValidAddress = addressPattern.test(address);
+        var isValidPhoneNumber = phoneNumberPattern.test(phoneNumber);
+
+        if (isValidName && isValidAddress && isValidPhoneNumber) {
+            // Create a new row in the table
+            // ...
+
+            // Clear input fields
+            nameInput.val("");
+            addressInput.val("");
+            phoneNumberInput.val("");
+
+            // Remove red highlighting if previously applied
+            nameInput.removeClass("invalid");
+            addressInput.removeClass("invalid");
+            phoneNumberInput.removeClass("invalid");
+        } else {
+            // Apply red highlighting to invalid input fields
+            if (!isValidName) {
+                nameInput.addClass("invalid");
+            }
+            if (!isValidAddress) {
+                addressInput.addClass("invalid");
+            }
+            if (!isValidPhoneNumber) {
+                phoneNumberInput.addClass("invalid");
+            }
+            alert("Please enter valid input for all fields.");
+        }
+    });
 });
+
+
+
+
 
